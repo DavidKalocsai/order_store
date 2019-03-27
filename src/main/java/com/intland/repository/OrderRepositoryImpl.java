d08570e78fcfc1cbce6360770657d1905e065ca9
David Kalocsai <kalocsaidavid@gmail.com>
2019-03-28 00:25:18 +0100

    Repository validates result
    When update, delete, add returns, returned values is validated.

--- src/main/java/com/intland/repository/OrderRepositoryImpl.java
+++ src/main/java/com/intland/repository/OrderRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.intland.repository;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -9,12 +8,13 @@ import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import com.intland.model.Order;
-import com.intland.model.OrderId;
 import com.intland.model.OrderDbObj;
+import com.intland.model.OrderId;
 import com.intland.repository.util.cast.OrderRowMapperResultExtractor;
 import com.intland.repository.util.names.ProcedureNames;
 import com.intland.repository.util.procedure.OrderProcedureParameterBuilder;
 import com.intland.repository.util.procedure.SimpleJdbcCreator;
+import com.intland.repository.validate.ProcedureResultValidator;
 
 @Transactional
 @Repository("orderRepository")
@@ -29,15 +29,22 @@ public class OrderRepositoryImpl implements OrderRepository {
   @Autowired
   private OrderRowMapperResultExtractor rowMapperResultExtractor;
 
+  @Autowired
+  private ProcedureResultValidator resultValidator;
+
 
   @Override
   public Optional<OrderDbObj> addOrder(final Order order) {
-    return processCall(ProcedureNames.getAddOrder(), parameterBuilder.getAddParameters(order));
+    return processModification(ProcedureNames.getAddOrder(),
+        parameterBuilder.getAddParameters(order));
   }
 
   @Override
   public Optional<OrderDbObj> getOrder(final OrderId id) {
-    return processCall(ProcedureNames.getGetOrder(), parameterBuilder.getGetParameters(id));
+    final SimpleJdbcCall simpleJdbcCall =
+        simpleJdbcCreator.createSimpleJdbc(ProcedureNames.getGetOrder());
+    return rowMapperResultExtractor
+        .extractOrder(simpleJdbcCall.execute(parameterBuilder.getGetParameters(id)));
   }
 
   @Override
@@ -49,20 +56,21 @@ public class OrderRepositoryImpl implements OrderRepository {
 
   @Override
   public Optional<OrderDbObj> updateOrder(final OrderDbObj order) {
-    return processCall(ProcedureNames.getUpdateOrder(),
+    return processModification(ProcedureNames.getUpdateOrder(),
         parameterBuilder.getUpdateParameters(order));
   }
 
   @Override
   public Optional<OrderDbObj> deleteOrder(final OrderDbObj order) {
-    return processCall(ProcedureNames.getDeleteOrder(),
+    return processModification(ProcedureNames.getDeleteOrder(),
         parameterBuilder.getDeleteParameters(order));
   }
 
-  private Optional<OrderDbObj> processCall(final String procedureName,
+  private Optional<OrderDbObj> processModification(final String procedureName,
       final SqlParameterSource paramters) {
     final SimpleJdbcCall simpleJdbcCall = simpleJdbcCreator.createSimpleJdbc(procedureName);
-    final Map<String, Object> out = simpleJdbcCall.execute(paramters);
-    return rowMapperResultExtractor.extractOrder(out);
+    final Optional<OrderDbObj> orderOptional =
+        rowMapperResultExtractor.extractOrder(simpleJdbcCall.execute(paramters));
+    return resultValidator.validate(orderOptional);
   }
 }
