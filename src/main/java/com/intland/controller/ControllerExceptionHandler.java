9ba41faf0b8c292810c4a0736cb2d9dee949d48b
David Kalocsai <kalocsaidavid@gmail.com>
2019-03-27 21:41:30 +0100

    Util ServiceError
    Util SeviceError was wrongly imported by ControllerExceptionHandler

--- src/main/java/com/intland/controller/ControllerExceptionHandler.java
+++ src/main/java/com/intland/controller/ControllerExceptionHandler.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import com.intland.util.ServiceError;
+import com.intland.controller.util.error.ServiceError;
 
 public abstract class ControllerExceptionHandler {
   private static final Logger LOG = LogManager.getLogger(ControllerExceptionHandler.class);
