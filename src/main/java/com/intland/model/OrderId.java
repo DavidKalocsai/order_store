b118e3628cef4a7a70ec71b22379ba13e27a6a2f
David Kalocsai <kalocsaidavid@gmail.com>
2019-03-28 00:23:14 +0100

    OrderId validation change
    OrderId group was validated as a number

--- src/main/java/com/intland/model/OrderId.java
+++ src/main/java/com/intland/model/OrderId.java
@@ -1,6 +1,7 @@
 package com.intland.model;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,7 +14,7 @@ public class OrderId {
   @Min(value = 1, message = "{id.id.invalid}")
   private Integer id;
 
-  @Min(value = 1, message = "{id.group.invalid=Group is invalid}")
+  @NotBlank(message = "{id.group.invalid}")
   private String group;
 
 }
