0c1eea7776237f1870641f7ecfabcd80af8b85de
David Kalocsai <kalocsaidavid@gmail.com>
2019-03-27 21:05:36 +0100

    Exception
    ValidationException is catched and converted into a 400 error,
    ServiceError object filled and passed back to client.

--- src/main/java/com/intland/controller/ControllerExceptionHandler.java
+++ src/main/java/com/intland/controller/ControllerExceptionHandler.java
@@ -1,11 +1,21 @@
 package com.intland.controller;
 
+import javax.validation.ValidationException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.intland.util.ServiceError;
 
-public class ControllerExceptionHandler {
+public abstract class ControllerExceptionHandler {
+  private static final Logger LOG = LogManager.getLogger(ControllerExceptionHandler.class);
+
+  @ExceptionHandler(ValidationException.class)
+  public ResponseEntity<ServiceError> handleValidationException(final RuntimeException ex) {
+    ServiceError error = new ServiceError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  }
 
   @ExceptionHandler(RuntimeException.class)
   public ResponseEntity<ServiceError> handle(RuntimeException ex) {
