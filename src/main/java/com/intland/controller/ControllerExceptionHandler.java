bf54c4349356ba2a613bd3b5cbe3a964ff131ff9
David Kalocsai <kalocsaidavid@gmail.com>
2019-03-28 00:18:35 +0100

    Controller Exception new Exception
    ControllerExceptoin Handler handle SqlCommandResultException.

--- src/main/java/com/intland/controller/ControllerExceptionHandler.java
+++ src/main/java/com/intland/controller/ControllerExceptionHandler.java
@@ -1,27 +1,38 @@
 package com.intland.controller;
 
-import com.intland.controller.util.error.ServiceError;
 import javax.validation.ValidationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.intland.controller.util.error.ServiceError;
+import com.intland.repository.exception.SqlCommandResultException;
 
 public abstract class ControllerExceptionHandler {
   private static final Logger LOG = LogManager.getLogger(ControllerExceptionHandler.class);
 
+  @ExceptionHandler(SqlCommandResultException.class)
+  public ResponseEntity<ServiceError> handleSqlCommandException(
+      final SqlCommandResultException ex) {
+    LOG.error("SqlCommandResultException {}", ex.getMessage());
+    return buildResponse(ex.getMessage());
+  }
+
   @ExceptionHandler(ValidationException.class)
-  public ResponseEntity<ServiceError> handleValidationException(final RuntimeException ex) {
-    ServiceError error = new ServiceError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
-    LOG.error("ValidationException {}", error);
-    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+  public ResponseEntity<ServiceError> handleValidationException(final ValidationException ex) {
+    LOG.error("ValidationException {}", ex.getMessage());
+    return buildResponse(ex.getMessage());
   }
 
   @ExceptionHandler(RuntimeException.class)
   public ResponseEntity<ServiceError> handle(RuntimeException ex) {
-    ServiceError error = new ServiceError(HttpStatus.OK.value(), ex.getMessage());
-    LOG.error("RuntimeException {}", error);
-    return new ResponseEntity<>(error, HttpStatus.OK);
+    LOG.error("RuntimeException {}", ex.getMessage());
+    return buildResponse(ex.getMessage());
+  }
+
+  private ResponseEntity<ServiceError> buildResponse(final String message) {
+    ServiceError error = new ServiceError(HttpStatus.BAD_REQUEST.value(), message);
+    return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
   }
 }
