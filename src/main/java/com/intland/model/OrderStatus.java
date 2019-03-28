5cb591976b6e45d4c074d333d86beeb64e0fc24e
David Kalocsai <kalocsaidavid@gmail.com>
2019-03-28 01:36:39 +0100

    Getter, javadoc added to OrderStatus
    Enumaration convert added to separate converted class.

--- src/main/java/com/intland/model/OrderStatus.java
+++ src/main/java/com/intland/model/OrderStatus.java
@@ -1,7 +1,13 @@
 package com.intland.model;
 
 import com.google.common.base.MoreObjects;
+import lombok.Getter;
 
+/**
+ * Status of an order. It can be active, or deleted. On delete order is only set to deleted, but not
+ * removed.
+ */
+@Getter
 public enum OrderStatus {
   ACTIVE("active", "Order is active!"), DELETED("deleted", "Order is deleted");
 
@@ -12,25 +18,6 @@ public enum OrderStatus {
     this.code = code;
     this.description = description;
   }
-
-  public String getCode() {
-    return code;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public static OrderStatus getEnumFromCode(final String code) {
-    OrderStatus orderStatus = null;
-    for (final OrderStatus o : OrderStatus.values()) {
-      if (orderStatus == null && o.getCode().equals(code)) {
-        orderStatus = o;
-      }
-    }
-    return orderStatus;
-  }
-
 
   @Override
   public String toString() {
