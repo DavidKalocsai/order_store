2a1753f55b1b866cc08ce3bdab84df1ae1efb436
David Kalocsai <kalocsaidavid@gmail.com>
2019-03-28 01:37:04 +0100

    Rowmapper uses new converter
    OrderStatus is converted from String in RowMapper

--- src/main/java/com/intland/repository/util/mapper/OrderRowMapper.java
+++ src/main/java/com/intland/repository/util/mapper/OrderRowMapper.java
@@ -2,14 +2,18 @@ package com.intland.repository.util.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import com.intland.model.Order;
-import com.intland.model.OrderStatus;
 import com.intland.model.OrderDbObj;
+import com.intland.model.OrderStatus;
 import com.intland.repository.util.names.GroupTablePropertyNames;
 import com.intland.repository.util.names.OrderTablePropertyNames;
 
 public class OrderRowMapper implements RowMapper<Order> {
+
+  @Autowired
+  private Converter<String, OrderStatus> orderStatusConverter;
 
   @Override
   public OrderDbObj mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -19,7 +23,7 @@ public class OrderRowMapper implements RowMapper<Order> {
     order.setDate(rs.getDate(OrderTablePropertyNames.getOrderDate()));
     order.setDescription(rs.getString(OrderTablePropertyNames.getOrderDesc()));
     order.setStatus(
-        OrderStatus.getEnumFromCode(rs.getString(OrderTablePropertyNames.getOrderStatus())));
+        orderStatusConverter.convert(rs.getString(OrderTablePropertyNames.getOrderStatus())));
     order.setVersion(rs.getInt(OrderTablePropertyNames.getOrderVersion()));
     return order;
   }
